3f54562a6d8780d6149623cc120b41e8b355e21d
grwadl <youngwwad@gmail.com>
2023-01-12 22:35:40 +0200

    installed package react-redux and wrapped app component into redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { ThemeProvider } from '@mui/material'
+import { Provider } from 'react-redux'
 import Header from './components/header/Header'
 import { theme } from './material.theme'
+import { store } from './redux'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App" id="app">
-        <Header />
-      </div>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <div className="App" id="app">
+          <Header />
+        </div>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
