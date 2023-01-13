660b5bbb9c03dc012497242664cc610ff7c3d174
grwadl <youngwwad@gmail.com>
2023-01-13 19:46:02 +0200

    removed extra hook and query observer

--- src/components/isolated/QueryObserver.tsx
+++
@@ -1,9 +0,0 @@
-import { useFetchBlogsOnChangedQuery } from '@/hooks/useFetchBlogsOnChangedQuery'
-
-//made this component instead of wrapping components in memo() to prevent extra call of the query reducer
-const QueryObserver = () => {
-  useFetchBlogsOnChangedQuery()
-  return null
-}
-
-export { QueryObserver }
