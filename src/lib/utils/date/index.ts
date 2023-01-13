10f618865a07f2af07bd380987bca08fa0ce8d41
grwadl <youngwwad@gmail.com>
2023-01-14 00:14:20 +0200

    fixed bug with the converting to the ordinal numbers

--- src/lib/utils/date/index.ts
+++ src/lib/utils/date/index.ts
@@ -5,5 +5,6 @@ export const ISOdateToHumanReadable = (dateISO: string): string => {
 
   const date = new Date(dateISO).toLocaleString('en-GB', { dateStyle: 'long' })
   const [day, month, year] = date.split(' ')
+
   return `${month} ${cardinalToOrdinalNumber(day)}, ${year}`
 }
