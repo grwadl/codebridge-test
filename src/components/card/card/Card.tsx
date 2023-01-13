c45bb60d638052e224e554b8f869eccd5844064c
grwadl <youngwwad@gmail.com>
2023-01-13 22:43:40 +0200

    added text-highlighting

--- src/components/card/card/Card.tsx
+++ src/components/card/card/Card.tsx
@@ -1,7 +1,7 @@
 import placeholder from '@/assets/placeholder.png'
 import { LazyLoad } from '@/components/lazy-load/LazyLoad'
 import { Blog } from '@/lib/services/api'
-import { ISOdateToHumanReadable } from '@/lib/utils'
+import { ISOdateToHumanReadable, highlightText } from '@/lib/utils'
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt'
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
 import { Card, CardContent } from '@mui/material'
@@ -24,8 +24,8 @@ const BlogCard = (props: Props) => {
             <CalendarTodayIcon height="13px" />
             <span className="date-text">{date}</span>
           </div>
-          <h5 className="card-title">{title}</h5>
-          <p className="card-summary">{summary}</p>
+          <h5 className="card-title">{highlightText(title, search)}</h5>
+          <p className="card-summary">{highlightText(summary, search)}</p>
           <div className="learn-more-wrap">
             <span className="learn-more-text">Learn more</span>
             <ArrowRightAltIcon className="learn-more-arrow" />
