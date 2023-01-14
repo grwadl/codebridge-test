17431727869d19a8626d8c53bf85f4c947127419
grwadl <youngwwad@gmail.com>
2023-01-14 10:40:10 +0200

    fixed function with convertion cardinal to ordeal numbers

--- src/lib/utils/number-order/cardinal-to-ordinal.ts
+++ src/lib/utils/number-order/cardinal-to-ordinal.ts
@@ -1,10 +1,16 @@
 import { Ordinal } from './types/ordinal.enum'
-
+//TODO rewrite
 export const cardinalToOrdinalNumber = (cardinal: string): string => {
   if (!cardinal) return ''
 
   const lastNumber = String(cardinal).charAt(cardinal.length - 1)
+  const firstNumber = String(cardinal).charAt(0)
+
+  const teenth = String(cardinal).length === 2 && firstNumber === '1'
+
+  if (teenth) return cardinal + 'th'
+
   if (!(lastNumber in Ordinal)) return cardinal + 'th'
 
-  return lastNumber + Ordinal[+lastNumber as keyof typeof Ordinal]
+  return cardinal + Ordinal[+lastNumber as keyof typeof Ordinal]
 }
